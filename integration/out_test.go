9451cc27f2b67200957e713f86bc9244e24396da
Bishoy Youssef <byoussef@pivotal.io>
2018-12-10 18:20:01 -0500

    refactor out_test
    Signed-off-by: Sameer Vohra <svohra@pivotal.io>
    Co-authored-by: Sameer Vohra <svohra@pivotal.io>

--- integration/out_test.go
+++ integration/out_test.go
@@ -3,6 +3,7 @@ package integration_test
 import (
 	"bytes"
 	"encoding/json"
+	"net/http"
 	"os/exec"
 	"strconv"
 	"time"
@@ -24,14 +25,25 @@ var _ = Describe("Out", func() {
 		input                         concourse.OutRequest
 		marshalledInput               []byte
 		err                           error
-		statusCode                    int
 		outResponse                   concourse.OutResponse
+		inputSource                   concourse.Source
+		statusHandlers                []http.HandlerFunc
 	)
+	BeforeEach(func() {
+		pipelineName = "foo"
+		applicationName = "bar"
+		inputSource = concourse.Source{
+			SpinnakerAPI:         spinnakerServer.URL(),
+			SpinnakerApplication: applicationName,
+			SpinnakerPipeline:    pipelineName,
+			X509Cert:             serverCert,
+			X509Key:              serverKey,
+		}
+		pipelineExecutionID = "ABC123"
 
-	JustBeforeEach(func() {
 		spinnakerServer.AppendHandlers(
 			ghttp.CombineHandlers(
-				ghttp.VerifyRequest("GET", MatchRegexp(".*/applications/"+applicationName)),
+				ghttp.VerifyRequest("GET", MatchRegexp(".*/applications/"+inputSource.SpinnakerApplication)),
 				ghttp.RespondWithJSONEncoded(
 					200,
 					map[string]interface{}{
@@ -44,43 +56,37 @@ var _ = Describe("Out", func() {
 					},
 				)),
 			ghttp.CombineHandlers(
-				ghttp.VerifyRequest("GET", MatchRegexp(".*/applications/"+applicationName+"/pipelineConfigs")),
+				ghttp.VerifyRequest("GET", MatchRegexp(".*/applications/"+inputSource.SpinnakerApplication+"/pipelineConfigs")),
 				ghttp.RespondWithJSONEncoded(
 					200,
 					[]map[string]string{
 						{"name": pipelineName},
 					},
 				)),
-			ghttp.CombineHandlers(
-				ghttp.VerifyRequest("POST", MatchRegexp(".*/pipelines/"+applicationName+"/"+pipelineName+".*")),
-				ghttp.RespondWithJSONEncoded(
-					statusCode,
-					responseMap,
-				),
-			),
 		)
+
+	})
+	JustBeforeEach(func() {
 		input = concourse.OutRequest{
-			Source: concourse.Source{
-				SpinnakerAPI:         spinnakerServer.URL(),
-				SpinnakerApplication: applicationName,
-				SpinnakerPipeline:    pipelineName,
-				X509Cert:             serverCert,
-				X509Key:              serverKey,
-			},
+			Source: inputSource,
 		}
 		marshalledInput, err = json.Marshal(input)
 		Expect(err).ToNot(HaveOccurred())
+		spinnakerServer.AppendHandlers(statusHandlers...)
 	})
 
 	Context("when Spinnaker responds with an accepted pipeline execution", func() {
 		BeforeEach(func() {
-			pipelineName = "foo"
-			applicationName = "bar"
-			pipelineExecutionID = "ABC123"
-			responseMap = map[string]string{
-				"ref": "/pipelines/" + pipelineExecutionID,
-			}
-			statusCode = 202
+			httpPOSTSuccessHandler := ghttp.CombineHandlers(
+				ghttp.VerifyRequest("POST", MatchRegexp(".*/pipelines/"+inputSource.SpinnakerApplication+"/"+pipelineName+".*")),
+				ghttp.RespondWithJSONEncoded(
+					202,
+					map[string]string{
+						"ref": "/pipelines/" + pipelineExecutionID,
+					},
+				),
+			)
+			spinnakerServer.AppendHandlers(httpPOSTSuccessHandler)
 		})
 
 		It("returns the pipeline execution id", func() {
@@ -94,172 +100,147 @@ var _ = Describe("Out", func() {
 			err = json.Unmarshal(outSess.Out.Contents(), &outResponse)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(outResponse.Version.Ref).To(Equal(pipelineExecutionID))
-
 		})
-		//TODO needs drying up
-		Context("when a status and timeout are specified and Spinnaker pipeline doesn't reach the desired state within the timeout duration", func() {
-			JustBeforeEach(func() {
-				input = concourse.OutRequest{
-					Source: concourse.Source{
-						SpinnakerAPI:         spinnakerServer.URL(),
-						SpinnakerApplication: applicationName,
-						SpinnakerPipeline:    pipelineName,
-						X509Cert:             serverCert,
-						X509Key:              serverKey,
-						Statuses:             []string{"SUCCEEDED"},
-						StatusCheckTimeout:   500 * time.Millisecond,
-						StatusCheckInterval:  200 * time.Millisecond,
-					},
-				}
-				runningHandler := ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
-					ghttp.RespondWithJSONEncoded(
-						statusCode,
-						map[string]string{
-							"id":     pipelineExecutionID,
-							"status": "RUNNING",
-						},
-					),
-				)
-				marshalledInput, err = json.Marshal(input)
-				Expect(err).ToNot(HaveOccurred())
-				spinnakerServer.AppendHandlers(
-					runningHandler,
-					runningHandler,
-					runningHandler,
-				)
+		Context("when status is configured", func() {
+			BeforeEach(func() {
+				inputSource.Statuses = []string{"SUCCEEDED"}
+				inputSource.StatusCheckInterval = 200 * time.Millisecond
 			})
 
-			It("times out and exits with a non zero status and prints an error message", func() {
-				cmd := exec.Command(outPath)
-				cmd.Stdin = bytes.NewBuffer(marshalledInput)
-				outSess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).ToNot(HaveOccurred())
-				Eventually(outSess.Exited).Should(BeClosed())
-				// Expect(spinnakerServer.ReceivedRequests()).Should(HaveLen(6))
-				Expect(outSess.ExitCode()).To(Equal(1))
+			Context("when a status and timeout are specified and Spinnaker pipeline doesn't reach the desired state within the timeout duration", func() {
+				BeforeEach(func() {
+					inputSource.StatusCheckTimeout = 500 * time.Millisecond
 
-				Expect(outSess.Err).To(gbytes.Say("error put step failed: "))
-				Expect(outSess.Err).To(gbytes.Say("timed out waiting for configured status\\(es\\)"))
-			})
-		})
-		//TODO needs drying up
-		Context("when a status is specified, and an unexpected final status reached", func() {
-			JustBeforeEach(func() {
-				input = concourse.OutRequest{
-					Source: concourse.Source{
-						SpinnakerAPI:         spinnakerServer.URL(),
-						SpinnakerApplication: applicationName,
-						SpinnakerPipeline:    pipelineName,
-						X509Cert:             serverCert,
-						X509Key:              serverKey,
-						Statuses:             []string{"SUCCEEDED"},
-						StatusCheckInterval:  200 * time.Millisecond,
-					},
-				}
-				marshalledInput, err = json.Marshal(input)
-				Expect(err).ToNot(HaveOccurred())
-				spinnakerServer.AppendHandlers(
-					ghttp.CombineHandlers(
+					runningHandler := ghttp.CombineHandlers(
 						ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
 						ghttp.RespondWithJSONEncoded(
-							statusCode,
+							200,
 							map[string]string{
 								"id":     pipelineExecutionID,
 								"status": "RUNNING",
 							},
 						),
-					),
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
-						ghttp.RespondWithJSONEncoded(
-							statusCode,
-							map[string]string{
-								"id":     pipelineExecutionID,
-								"status": "TERMINAL",
-							},
+					)
+					statusHandlers = []http.HandlerFunc{
+						runningHandler,
+						runningHandler,
+						runningHandler,
+					}
+				})
+
+				It("times out and exits with a non zero status and prints an error message", func() {
+					cmd := exec.Command(outPath)
+					cmd.Stdin = bytes.NewBuffer(marshalledInput)
+					outSess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					Eventually(outSess.Exited).Should(BeClosed())
+					Expect(outSess.ExitCode()).To(Equal(1))
+
+					Expect(outSess.Err).To(gbytes.Say("error put step failed: "))
+					Expect(outSess.Err).To(gbytes.Say("timed out waiting for configured status\\(es\\)"))
+				})
+			})
+			Context("when a status is specified, and an unexpected final status reached", func() {
+				BeforeEach(func() {
+					statusHandlers := []http.HandlerFunc{
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
+							ghttp.RespondWithJSONEncoded(
+								200,
+								map[string]string{
+									"id":     pipelineExecutionID,
+									"status": "RUNNING",
+								},
+							),
 						),
-					),
-				)
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
+							ghttp.RespondWithJSONEncoded(
+								200,
+								map[string]string{
+									"id":     pipelineExecutionID,
+									"status": "TERMINAL",
+								},
+							),
+						),
+					}
+					spinnakerServer.AppendHandlers(statusHandlers...)
+				})
+
+				It("exits with non zero code and prints an error message", func() {
+					cmd := exec.Command(outPath)
+					cmd.Stdin = bytes.NewBuffer(marshalledInput)
+					outSess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					<-outSess.Exited
+					Expect(spinnakerServer.ReceivedRequests()).Should(HaveLen(5))
+					Expect(outSess.ExitCode()).To(Equal(1))
+
+					Expect(outSess.Err).To(gbytes.Say("error put step failed:"))
+					Expect(outSess.Err).To(gbytes.Say("Pipeline execution reached a final state: TERMINAL"))
+				})
 			})
 
-			It("exits with non zero code and prints an error message", func() {
-				cmd := exec.Command(outPath)
-				cmd.Stdin = bytes.NewBuffer(marshalledInput)
-				outSess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).ToNot(HaveOccurred())
-				<-outSess.Exited
-				Expect(spinnakerServer.ReceivedRequests()).Should(HaveLen(5))
-				Expect(outSess.ExitCode()).To(Equal(1))
-
-				Expect(outSess.Err).To(gbytes.Say("error put step failed:"))
-				Expect(outSess.Err).To(gbytes.Say("Pipeline execution reached a final state: TERMINAL"))
-			})
-		})
-
-		//TODO needs drying up
-		Context("when a status is specified, and reached", func() {
-			JustBeforeEach(func() {
-				input = concourse.OutRequest{
-					Source: concourse.Source{
-						SpinnakerAPI:         spinnakerServer.URL(),
-						SpinnakerApplication: applicationName,
-						SpinnakerPipeline:    pipelineName,
-						X509Cert:             serverCert,
-						X509Key:              serverKey,
-						Statuses:             []string{"SUCCEEDED"},
-						StatusCheckInterval:  200 * time.Millisecond,
-					},
-				}
-				marshalledInput, err = json.Marshal(input)
-				Expect(err).ToNot(HaveOccurred())
-				spinnakerServer.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
-						ghttp.RespondWithJSONEncoded(
-							statusCode,
-							map[string]string{
-								"id":     pipelineExecutionID,
-								"status": "RUNNING",
-							},
+			//TODO needs drying up
+			Context("when a status is specified, and reached", func() {
+				BeforeEach(func() {
+					statusHandlers = []http.HandlerFunc{
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
+							ghttp.RespondWithJSONEncoded(
+								200,
+								map[string]string{
+									"id":     pipelineExecutionID,
+									"status": "RUNNING",
+								},
+							),
 						),
-					),
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
-						ghttp.RespondWithJSONEncoded(
-							statusCode,
-							map[string]string{
-								"id":     pipelineExecutionID,
-								"status": "SUCCEEDED",
-							},
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("GET", MatchRegexp(".*/pipelines/"+pipelineExecutionID+".*")),
+							ghttp.RespondWithJSONEncoded(
+								200,
+								map[string]string{
+									"id":     pipelineExecutionID,
+									"status": "SUCCEEDED",
+								},
+							),
 						),
-					),
-				)
-			})
-			It("waits till the pipeline execution status is satisfied and returns the pipeline execution id", func() {
-				cmd := exec.Command(outPath)
-				cmd.Stdin = bytes.NewBuffer(marshalledInput)
-				outSess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-				Expect(err).ToNot(HaveOccurred())
-				<-outSess.Exited
-				Expect(spinnakerServer.ReceivedRequests()).Should(HaveLen(5))
-				Expect(outSess.ExitCode()).To(Equal(0))
+					}
+				})
+				It("waits till the pipeline execution status is satisfied and returns the pipeline execution id", func() {
+					cmd := exec.Command(outPath)
+					cmd.Stdin = bytes.NewBuffer(marshalledInput)
+					outSess, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+					Expect(err).ToNot(HaveOccurred())
+					<-outSess.Exited
+					Expect(spinnakerServer.ReceivedRequests()).Should(HaveLen(5))
+					Expect(outSess.ExitCode()).To(Equal(0))
 
-				err = json.Unmarshal(outSess.Out.Contents(), &outResponse)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(outResponse.Version.Ref).To(Equal(pipelineExecutionID))
+					err = json.Unmarshal(outSess.Out.Contents(), &outResponse)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(outResponse.Version.Ref).To(Equal(pipelineExecutionID))
+				})
 			})
 		})
 	})
 
-	Context("when spinnaker responds with 4xx", func() {
+	Context("when Spinnaker responds with 4xx on a POST for a pipeline execution", func() {
+		var statusCode int
 		BeforeEach(func() {
-			pipelineName = "foo"
-			applicationName = "bar"
+
+			statusCode = 422
 			responseMap = map[string]string{
 				"message": "500 ",
 			}
-			statusCode = 422
+			httpPOSTFailureHandler := ghttp.CombineHandlers(
+				ghttp.VerifyRequest("POST", MatchRegexp(".*/pipelines/"+inputSource.SpinnakerApplication+"/"+pipelineName+".*")),
+				ghttp.RespondWithJSONEncoded(
+					statusCode,
+					responseMap,
+				),
+			)
+			spinnakerServer.AppendHandlers(httpPOSTFailureHandler)
+
 		})
 
 		It("prints the status code, response body and exits with exit code 1", func() {
